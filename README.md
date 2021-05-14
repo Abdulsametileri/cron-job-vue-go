dbaf765b9d75de61c96941702e927e8e806e7451
Abdulsametileri <sametileri07@gmail.com>
2021-05-14 20:03:06 +0300

    vue boilerplate created

---
+++ README.md
@@ -0,0 +1,4 @@
+### To Do List
+
+- [ ] Prepare Web UI on Vue.js Side. We can get the users
+image file, time and date. /add route. 
---
+++ client/src/views/AlarmList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+     <h1>Listele</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AlarmList",
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
