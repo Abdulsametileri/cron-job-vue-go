ead4364a4635b9c07e361e020b216933290a364d
Abdulsametileri <sametileri07@gmail.com>
2021-05-23 10:53:14 +0300

    config_dev json added for test purposes

--- config/config.go
+++ config/config.go
@@ -6,7 +6,11 @@ import (
 )
 
 func Setup() {
-	viper.SetConfigFile(".env")
+	if IsDebug {
+		viper.SetConfigFile("config_dev.json")
+	} else {
+		viper.SetConfigFile(".env")
+	}
 
 	if err := viper.ReadInConfig(); err != nil {
 		log.Fatalf("Error while reading config file %s", err)
